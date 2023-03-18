1de04bedac336eeeaafe717183418a231769f85c
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-03-18 08:15:03 -0300

    ATUALIZAR COMPONENTES

--- meu-site/src/App.js
+++ meu-site/src/App.js
@@ -1,33 +1,35 @@
 /* import logo from './logo.svg'; */
 import './App.css';
-
+import HelloWorld from './componentes/Hello.Word';
 
 function App() {
 
-  /*  const name = 'PAULO'
+  const name = 'PAULO'
 
-  const newName = name.toLocaleUpperCase() */
+  const newName = name.toLocaleUpperCase()
 
- /*  export default function sum(a,b){
+  function sum(a,b){
     return a + b
-  }  */
+  }
 
-/*   const url = 'https://via.placeholder.com/150'  */
+  const url = 'https://via.placeholder.com/150'
 
   return (
     /*Sempre tem que ter um elemento pai*/
     /*Sempre usa className no react e não class porque é uma palavra reservada */
-     <div className="App">
+    <div className="App">
      {
       <div>
       <h1>Hello World</h1>
-{/*       <p>Meu primeiro App {name}</p>
- */}{/*       <p>Some: {sum(1,2)}</p>
-/*  */}     {/*  <img src={url} alt="Minha Imagem"/> */}
- */      </div>
+      <p>Meu primeiro App {name}</p>
+      <p>Some: {sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem"/>
+      <HelloWorld/>
+      </div>
       }
-    </div> 
+    </div>
     /*Imagem dinimica com função url*/
   );
 }
 
+export default App
---
+++ meu-site/src/componentes/Hello.Word.js
@@ -0,0 +1,11 @@
+ function HelloWorld(){
+
+    return (
+        <div>
+            <h1>Meu Primeiro Componente</h1>
+        </div>
+    )
+
+}
+
+export default HelloWorld 
