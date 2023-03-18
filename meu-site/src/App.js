5d88cd2837d932ea5c7b5fabbb8991026ac341ab
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-03-18 07:42:18 -0300

    atualizar

--- meu-site/src/App.js
+++ meu-site/src/App.js
@@ -1,31 +1,32 @@
 /* import logo from './logo.svg'; */
 import './App.css';
 
+
 function App() {
 
-  const name = 'PAULO'
+  /*  const name = 'PAULO'
 
-  const newName = name.toLocaleUpperCase()
+  const newName = name.toLocaleUpperCase() */
 
-  function sum(a,b){
+ /*  export default function sum(a,b){
     return a + b
-  }
+  }  */
 
-  const url = 'https://via.placeholder.com/150'
+/*   const url = 'https://via.placeholder.com/150'  */
 
   return (
     /*Sempre tem que ter um elemento pai*/
     /*Sempre usa className no react e não class porque é uma palavra reservada */
-    <div className="App">
+     <div className="App">
      {
       <div>
       <h1>Hello World</h1>
-      <p>Meu primeiro App {name}</p>
-      <p>Some: {sum(1,2)}</p>
-      <img src={url} alt="Minha Imagem"/>
-      </div>
+{/*       <p>Meu primeiro App {name}</p>
+ */}{/*       <p>Some: {sum(1,2)}</p>
+/*  */}     {/*  <img src={url} alt="Minha Imagem"/> */}
+ */      </div>
       }
-    </div>
+    </div> 
     /*Imagem dinimica com função url*/
   );
 }
